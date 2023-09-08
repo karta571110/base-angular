37c385bf50a65150db14e49d8db736ec5c07fb35
Ming Hsi Wang <karta571110@gmail.com>
2023-09-08 21:52:47 +0800

    work temp

--- libs/common-lib/src/lib/common-lib.service.ts
+++ libs/common-lib/src/lib/common-lib.service.ts
@@ -4,6 +4,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CommonLibService {
+  ab: any;
+
+  bs: any;
+
   constructor() {
     console.log('CommonLibService');
   }
