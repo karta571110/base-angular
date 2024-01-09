41e86b63e2cf255d2709cb75a4e61bd220a142cd
Ted Wang <karta571110@gmail.com>
2024-01-09 17:34:29 +0800

    chore: add pwa package

--- apps/pwa-app/src/app/app.config.ts
+++ apps/pwa-app/src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { getTranslocoProvide } from '@common/sdk/i18n';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ...getTranslocoProvide()],
+  providers: [
+    provideRouter(routes),
+    ...getTranslocoProvide(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
