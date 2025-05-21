d604d70bbf63a4290fd0fe48f8e9e666ff211a1d
Ted Wang <karta571110@gmail.com>
2025-05-21 17:45:49 +0800

    feat: add table

--- apps/base-app/src/app/shared/components/table/table.component.ts
+++ apps/base-app/src/app/shared/components/table/table.component.ts
@@ -1,19 +1,21 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { NgTemplateOutlet } from '@angular/common';
+import { NgStyle, NgTemplateOutlet } from '@angular/common';
 import {
   AfterContentInit,
   Component,
   computed,
   contentChildren,
   input,
-  OnDestroy,
   output,
   TemplateRef,
 } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ContentViewChildTypePipe } from '@common/sdk';
-import { TranslocoModule } from '@jsverse/transloco';
+import isNil from 'lodash-es/isNil';
+import { ButtonComponent } from '../buttons/button/button.component';
+import { PaginatorComponent } from '../paginator/paginator.component';
 import { CustomTdDirective } from './directives/custom-td/custom-td.directive';
 import { CustomThDirective } from './directives/custom-th/custom-th.directive';
 import { Column } from './table.model';
@@ -22,39 +24,77 @@ import { Column } from './table.model';
   imports: [
     ScrollingModule,
     MatTableModule,
-    TranslocoModule,
     NgTemplateOutlet,
     ContentViewChildTypePipe,
+    NgStyle,
+    ButtonComponent,
+    PaginatorComponent,
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent<DataT> implements AfterContentInit, OnDestroy {
-  rowData = input<DataT[]>([]);
+export class TableComponent<DataT> implements AfterContentInit {
+  rowData = input.required<DataT[]>();
 
-  columns = input<Column[]>([]);
+  columns = input.required<Column[]>();
 
   isSticky = input(false);
 
+  usePaginator = input(true);
+
   rowClick = output<DataT>();
+
+  columnSortClick = output<Column>();
+
+  pageEvent = output<PageEvent>();
+
+  protected sortModeStep = ['swap_vert', 'south', 'north'] as const;
 
   protected displayedColumns = computed(() =>
     this.columns().map(column => column.field)
   );
 
-  protected dataSource = new MatTableDataSource(this.rowData());
+  protected dataSource = new MatTableDataSource<DataT>([]);
 
-  protected rowTemplateMap = new Map<string, TemplateRef<DataT>>();
+  protected rowTemplateMap = new Map<
+    string,
+    TemplateRef<DataT & { $implicit: DataT }>
+  >();
+
+  protected columnsForRender = computed(() =>
+    this.columns().map(column => ({
+      $implicit: column,
+      ...column,
+    }))
+  );
+
+  protected columnSettingKeyMap = computed(() => {
+    const map = new Map<string, Column>();
+    const columns = this.columns();
+
+    columns.forEach(column => {
+      map.set(column.field, column);
+    });
+
+    return map;
+  });
 
   protected columnTemplateMap = new Map<string, TemplateRef<Column>>();
-
-  private _rowData$ = toObservable(this.rowData).subscribe(data => {
-    this.dataSource.data = data;
-  });
 
   private _tdTemplateRefs = contentChildren(CustomTdDirective);
 
   private _thTemplateRefs = contentChildren(CustomThDirective);
+
+  constructor() {
+    toObservable(this.rowData)
+      .pipe(takeUntilDestroyed())
+      .subscribe(data => {
+        this.dataSource.data = data.map(d => ({
+          $implicit: d,
+          ...d,
+        }));
+      });
+  }
 
   ngAfterContentInit(): void {
     this._tdTemplateRefs().forEach(d => {
@@ -66,11 +106,17 @@ export class TableComponent<DataT> implements AfterContentInit, OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
-    this._rowData$.unsubscribe();
-  }
-
   protected rowClickEvent(data: DataT): void {
     this.rowClick.emit(data);
+  }
+
+  protected nextSortStep(column: Column): void {
+    if (!isNil(column.sort)) {
+      const sort = column.sort + 1;
+
+      column.sort =
+        sort >= this.sortModeStep.length ? 0 : (sort as Column['sort']);
+      this.columnSortClick.emit(column);
+    }
   }
 }
--- apps/base-app/src/app/shared/components/table/table.model.ts
+++ apps/base-app/src/app/shared/components/table/table.model.ts
@@ -1,6 +1,18 @@
-export interface Column {
+export interface Column<rowKey = string> {
   /** 資料欄位 */
-  field: string;
+  field: rowKey;
+
   /** 標頭名稱 */
   name: string;
+
+  /** 標頭及欄位文字位置 */
+  align?: 'center' | 'end' | 'start';
+
+  /**
+   * 排序模式
+   * 0: default
+   * 1: asc
+   * 2: desc
+   */
+  sort?: 0 | 1 | 2;
 }
